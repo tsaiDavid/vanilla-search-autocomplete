5290188e54e03b8fb43c914dd8a162978a9f68e6
David Tsai <tsaiDavid@users.noreply.github.com>
2018-08-22 11:54:54 -0700

    Adding search form to html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import { searchData as search } from "./mockSearch";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
