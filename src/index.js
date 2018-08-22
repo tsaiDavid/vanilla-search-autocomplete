308965bd4b023199e7f7143a4ddbc0d4cfd26104
David Tsai <tsaiDavid@users.noreply.github.com>
2018-08-22 12:10:08 -0700

    rendering returned results to list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import { searchData as search } from "./mockSearch";
+
+// For the sake of this example, we're safely assuming there will be no duplicate classNames
+const searchBar = document.getElementsByClassName("search-input")[0];
+const results = document.getElementsByClassName("results")[0];
+
+const noResult = document.createElement("li");
+noResult.innerText = "Sorry, no movies found";
+
+const renderList = function(movies, target) {
+  if (!movies.length) {
+    results.appendChild(noResult);
+  } else {
+    const list = movies.reduce((frag, movie) => {
+      const item = document.createElement("li");
+      item.innerText = `${movie.title} - ${movie.rating}`;
+      frag.appendChild(item);
+      return frag;
+    }, document.createDocumentFragment());
+    results.appendChild(list);
+  }
+};
+
+searchBar.oninput = e => {
+  search(e.target.value)
+    .then(res => renderList(res))
+    .catch(err => console.error(err));
+};
